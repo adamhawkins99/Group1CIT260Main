a94d2be7bb2d0fa73f938738ac0370488a85690c
admin <admin@dell-irenflow>
2017-10-07 23:22:52 +0300

    Creating Player class

---
+++ Group1 Main Game/src/mainGameModel/Game.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mainGameModel;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author admin
+ */
+public class Game implements Serializable{
+    
+    private double totalTime;
+    private int noPeople;
+    private Player player;
+    private Crops crops;
+
+    public Game() {
+    }
+    
+    
+
+    public double getTotalTime() {
+        return totalTime;
+    }
+
+    public void setTotalTime(double totalTime) {
+        this.totalTime = totalTime;
+    }
+
+    public int getNoPeople() {
+        return noPeople;
+    }
+
+    public void setNoPeople(int noPeople) {
+        this.noPeople = noPeople;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    @Override
+    public String toString() {
+        return "Game{" + "totalTime=" + totalTime + ", noPeople=" + noPeople + ", player=" + player + '}';
+    }
+    
+    
+    
+    
+}
