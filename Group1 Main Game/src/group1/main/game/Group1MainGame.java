40e85fc440ed08857af64330c75500f6808bef33
admin <admin@dell-irenflow>
2017-10-07 23:21:00 +0300

    Creating Player class

--- Group1 Main Game/src/group1/main/game/Group1MainGame.java
+++ Group1 Main Game/src/group1/main/game/Group1MainGame.java
@@ -5,6 +5,9 @@
  */
 package group1.main.game;
 
+import mainGameModel.Crops;
+import mainGameModel.Player;
+
 /**
  *
  * @author adam
@@ -15,7 +18,20 @@ public class Group1MainGame {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Player playerOne = new Player ();
+        
+        playerOne.setName("Irena Kyman");
+        
+        String  playerOneName = playerOne.getName();
+        
+        System.out.println(playerOne.toString());
+        
+        Crops currentYear = new Crops ();
+        
+        currentYear.setNewPeople(5);
+        currentYear.setNumberWhoDied(10);
+        
+        System.out.println(currentYear.toString());
     }
     
 }
