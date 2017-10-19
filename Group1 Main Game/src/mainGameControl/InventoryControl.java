ca3ba0ab7d9c99ed3c269dda3f6031e6209dbdf2
admin <admin@dell-irenflow>
2017-10-20 01:40:28 +0300

    InventoryControl+test

---
+++ Group1 Main Game/src/mainGameControl/InventoryControl.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mainGameControl;
+
+import mainGameModel.Crops;
+
+/**
+ *
+ * @author adam
+ */
+public class InventoryControl{
+    public int buyLand (int landPrice , int acresToBuy, Crops theCrops){
+
+       
+              
+        if (acresToBuy < 0){
+            return -1;
+        }
+        if (acresToBuy > 200){
+            return -1;
+        }
+        int wheat = theCrops.getWheatInStore();
+        if ((acresToBuy * landPrice) > wheat){
+            return -1;
+        }
+        int acres = theCrops.getAcres();
+        acres += acresToBuy;
+        wheat -= (acresToBuy * landPrice);
+        theCrops.setWheatInStore(wheat);
+        
+        return wheat;
+
+        }
+        
+    
+}
