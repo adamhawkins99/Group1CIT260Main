20388f4104553dd9f18310d9898d0bfa1bed6420
adamhawkins99 <32527229+adamhawkins99@users.noreply.github.com>
2017-11-11 11:14:28 -0800

    Map again

--- Group1 Main Game/src/mainGameModel/Map.java
+++ Group1 Main Game/src/mainGameModel/Map.java
@@ -9,6 +9,69 @@ package mainGameModel;
  *
  * @author adam
  */
-public class Map {
+public class Map implements Serializable{
+    private int noOfRows;
+    private int NoOfColumns;
+    private Location[][] locations;
+
+    public Map() {
+    }
+
+    public Map(int noOfRows, int noOfColumns) {
+        
+        if (noOfRows < 1 || noOfColumns < 1){
+        System.out.println("Please enter a value greater than zero.");
+        return;
+        }
+
+        this.noOfRows = noOfRows;
+        this.NoOfColumns = noOfColumns;
+        
+        //create 2-d array for Location objects
+        this.locations = new Location[noOfRows][NoOfColumns];
+        
+        for (int row = 0; row< noOfRows; row++){
+            for(int column = 0; column< noOfColumns; column++){
+                //create and initialize new Location object instance
+                Location location = new Location();
+                location.setColumn(column);
+                location.setRow(row);
+                location.setVisited(false);
+                
+                //assign the Location object to the current position in array
+                locations[row][column] = location;
+            }
+        }
+        
     
+    
+    }
+    
+  
+
+    public Location[][] getLocations() {
+        return locations;
+         }
+
+    public void setLocations(Location[][] locations) {
+        this.locations = locations;
+    }
+
+    public int getNoOfRows() {
+        return noOfRows;
+    }
+
+    public void setNoOfRows(int noOfRows) {
+        this.noOfRows = noOfRows;
+    }
+
+    public int getNoOfColumns() {
+        return NoOfColumns;
+    }
+
+    public void setNoOfColumns(int NoOfColumns) {
+        this.NoOfColumns = NoOfColumns;
+    }
+    
+
 }
